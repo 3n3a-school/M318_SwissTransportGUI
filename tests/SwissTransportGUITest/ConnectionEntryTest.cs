7cdd0f1e42cf1cb44cb4912edb5742d40156df9d
3n3a <info@3n3a.ch>
2022-02-09 08:35:11 +0100

    add model tests

---
+++ tests/SwissTransportGUITest/ConnectionEntryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SwissTransportGUI.Model;
+using FluentAssertions;
+using GMap.NET;
+using Xunit;
+
+namespace SwissTransportGUITest
+{
+    public class ConnectionEntryTest
+    {
+
+        [Fact]
+        public void ConnectionEntry()
+        {
+            ConnectionEntry test1 = new ConnectionEntry()
+            {
+                Duration = "30.05M",
+                FromStation = "Zürich",
+                FromStationDepartureTime = DateTime.Now,
+                ToStation = "luzern",
+                ToStationArrivalTime = DateTime.Now.AddHours(2),
+                FromStationCoord = new PointLatLng(0, 0),
+                ToStationCoord = new PointLatLng(20, 20),
+            };
+            test1.Should().BeOfType<ConnectionEntry>();
+        }
+    }
+}
